28ac4252f14a89992205b6464aafa262b4e3caab
sacphan <56332124+sacphan@users.noreply.github.com>
2020-12-16 17:21:22 +0700

    update tao phong

--- src/views/reports/DashboardView/index.js
+++ src/views/reports/DashboardView/index.js
@@ -7,9 +7,8 @@ import {
 import Page from 'src/components/Page';
 import Budget from './Budget';
 import BoardsContext from 'src/context/BoardsContext'
-
-import ChatSignalR from 'src/views/chat/Chat-SignalR'
-
+import APIManager from 'src/utils/LinkAPI'
+import SimpleModal from 'src/views/Plugin/modal'
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.background.dark,
@@ -20,10 +19,78 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Dashboard = () => {
-  return <ChatSignalR />;
+  const classes = useStyles();
+
+  const [boards, setBoards] = useState(Array());
+  let listBoard = [];
+  useEffect(() => {
+    async function fetchBoardList() {
+     
+      var token = JSON.parse(localStorage.getItem("Token")).token;
+    
+
+      const requestURL = APIManager+"/api/GetListBoardBlank";
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token
+        }
+      };
+      fetch(requestURL, requestOptions)
+        .then(response => response.json())
+        .then(data => setBoards(data.data));
+    }
+
+    fetchBoardList();
+  }, []);
+  const renderBoard = (item) => {
+
+    return <Grid key={item.id} item
+      lg={3}
+      sm={6}
+      xl={3}
+      xs={12}
+    >
+      <Budget board={item} />
+    </Grid>
+
+  }
+  
+  boards.map((item) => {
+
+    listBoard.push(renderBoard(item));
+  })
+
+  return (
+    <BoardsContext.Provider value={{
+      boards:boards,
+      setBoards:setBoards
+    }}>
+    <Page
+      className={classes.root}
+      title="Dashboard"
+    >
+      
+      <Container maxWidth={false}>
+      
+         <SimpleModal boards={boards} setBoards={setBoards} />
+       
+        <br />
+        <Grid
+          container
+          spacing={3}
+        >
+          {listBoard}
+          
+        </Grid>
+       
+       
+      </Container>
+      
+    </Page>
+    </BoardsContext.Provider>
+  );
 };
 
 export default Dashboard;
-
-
-
